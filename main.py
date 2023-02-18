216cf078241b1b34916560fd719741e6c875a555
novenopatch <homawoojoseph@gmail.com>
2023-02-18 16:09:28 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from pytube import YouTube
 from pytube import Playlist
 import os
 
-
-def _banner ():
-    print(""" __   __  _______    ______   _______  _     _  __    _ 
+WELCOME_STR = """ __   __  _______    ______   _______  _     _  __    _ 
     |  | |  ||       |  |      | |       || | _ | ||  |  | |
     |  |_|  ||_     _|  |  _    ||   _   || || || ||   |_| |
     |       |  |   |    | | |   ||  | |  ||       ||       |
@@ -17,31 +15,41 @@ def _banner ():
         1-) Download youtube video
         2-) Download youtube playliste
         
-        """)
+        """
+INVALID_LINK_MESSAGE = "Le lien n'est pas valide."
+RESOLUTIONS_MESSAGE = "Qualités disponibles "
+FILE_EXTENSION = 'mp4'
+CHOICE_QUALITY_MESSAGE = "Choisissez une qualité (entrez un nombre ou/ laisser vide pour utiliser la highest_resolution) "
+START_DOWNLOAD_MESSAGE = "-----------Début du téléchargement-----------"
+END_MESSAGE = "-----------Fin-----------"
+
+
+def _banner ():
+    print(WELCOME_STR)
 
 def validate_url(link:str) -> str:
     pattern = re.compile(r'(https?://)?(www\.)?youtube\.com/watch\?v=[\w-]+(&\S*)?|^(https?://)?(www\.)?youtu\.be/[\w-]+$')
     if pattern.match(link):
         return link
     else:
-        raise ValueError("Le lien n'est pas valide.")
+        raise ValueError(INVALID_LINK_MESSAGE)
 
 
 def video(link: str, output_path: str):
     youtube = YouTube(link,on_progress_callback=on_progress)
     print(youtube.title)
-    print("Début du téléchargement")
-    streams = youtube.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')
-    print("Qualités disponibles :")
+    streams = youtube.streams.filter(progressive=True, file_extension=FILE_EXTENSION).order_by('resolution')
+    print(RESOLUTIONS_MESSAGE+":")
     for i, stream in enumerate(streams):
         print(f"{i+1}) {stream.resolution}")
     try:
-        choice = input("Choisissez une qualité (entrez un nombre): ")
+        choice = input(CHOICE_QUALITY_MESSAGE+" ")
         if not choice:
             video = youtube.streams.get_highest_resolution()
         else:
             choice = int(choice)
             video = streams[choice-1]
+        print(START_DOWNLOAD_MESSAGE)
         video.download(output_path=output_path)
     except Exception as e:
         print(f"Le téléchargement a échoué pour {youtube.title}. Erreur: {str(e)}")
@@ -54,10 +62,10 @@ def playliste(link: str, output_path: str):
     playlist = Playlist(link)
     print(playlist.title)
     for video in playlist.videos:
-        print("-----------Début du téléchargement-----------")
+        print(START_DOWNLOAD_MESSAGE)
         try:
             print(video.title)
-            video_streams = video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution')
+            video_streams = video.streams.filter(progressive=True, file_extension=FILE_EXTENSION).order_by('resolution')
             if video_streams:
                 video_choice = video_streams[-1]
                 video_choice.download(output_path=output_path)
@@ -65,7 +73,7 @@ def playliste(link: str, output_path: str):
                 print('Aucune qualité vidéo disponible pour la vidéo', video.title)
         except:
             print('Erreur')
-        print("-----------Fin-----------")
+        print(END_MESSAGE)
 
 
 def on_progress(stream, chunk, bytes_remaining):
@@ -77,20 +85,20 @@ def on_progress(stream, chunk, bytes_remaining):
 
 def _Main():
     _banner()
-
     choice = int(input('Votre choix: '))
-    if choice == 1:
-        link = input("Lien de la vidéo : ")
-        output_path = input("Chemin de téléchargement (laisser vide pour utiliser le répertoire courant) : ").strip()
-        if not output_path:
-            output_path = os.getcwd()
+    link = input("Lien de la vidéo ou le Lien de la playliste : ")
+    output_path = input("Chemin de téléchargement (laisser vide pour utiliser le répertoire courant) : ").strip()
+    if not output_path:
+      output_path = os.getcwd()
+    try:
+      link = validate_url(link)
+      if choice == 1:
         video(link, output_path)
-    else:
-        link = input("Lien de la playliste : ")
-        output_path = input("Chemin de téléchargement (laisser vide pour utiliser le répertoire courant) : ").strip()
-        if not output_path:
-            output_path = os.getcwd()
+      else:
         playliste(link, output_path)
+    except Exception as e:
+      print(f"il semble avoir un souci avec votre lien . Erreur: {str(e)}")
+    
 
 
 if __name__ == '__main__':
